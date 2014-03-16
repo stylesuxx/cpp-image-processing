cf21409540bd100a39813c409def5cde0d3db5e3
stylesuxx <stylesuxx@gmail.com>
2014-03-16 13:22:52 +0100

    Draw black border around image.

--- main.cpp
+++ main.cpp
@@ -5,29 +5,61 @@ using namespace std;
 int main()
 {
 	int width = 5;
-	int height = 3;
+	int height = 5;
 	Image *test = new Image(width, height);
 	Pixel *pixel;
 
 	// Set first row of pixels to red
 	for(int i = 0; i < width; i++) {
-		pixel = test->getPixel(0, i);
+		pixel = test->getPixel(1, i);
 		pixel->setGreen(0);
 		pixel->setBlue(0);
 	}
 
 	// Set second row of pixels to green
 	for(int i = 0; i < width; i++) {
-		pixel = test->getPixel(1, i);
+		pixel = test->getPixel(2, i);
 		pixel->setRed(0);
 		pixel->setBlue(0);
 	}
 
 	// Set third row of pixels to blue
 	for(int i = 0; i < width; i++) {
-		pixel = test->getPixel(2, i);
+		pixel = test->getPixel(3, i);
 		pixel->setRed(0);
 		pixel->setGreen(0);
+	}
+
+	// Draw a black border on bottom
+	for(int i = 0; i < width; i++) {
+		pixel = test->getPixel(height-1, i);
+		pixel->setRed(0);
+		pixel->setGreen(0);
+		pixel->setBlue(0);
+	}
+
+	// Draw a black border on top
+	for(int i = 0; i < width; i++) {
+		pixel = test->getPixel(0, i);
+		pixel->setRed(0);
+		pixel->setGreen(0);
+		pixel->setBlue(0);
+	}
+
+	// Draw a black border on left
+	for(int i = 0; i < height; i++) {
+		pixel = test->getPixel(i, 0);
+		pixel->setRed(0);
+		pixel->setGreen(0);
+		pixel->setBlue(0);
+	}
+
+	// Draw a black border on right
+	for(int i = 0; i < width; i++) {
+		pixel = test->getPixel(i, width - 1);
+		pixel->setRed(0);
+		pixel->setGreen(0);
+		pixel->setBlue(0);
 	}
 
 	test->saveToPPM("test.ppm");
